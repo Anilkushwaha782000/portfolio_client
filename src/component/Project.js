5bb01b1fe5e66c824a80cb6df393d6260585dad7
Anilkushwaha782000 <89802345+Anilkushwaha782000@users.noreply.github.com>
2024-02-29 01:53:20 +0530

    Update Project.js

--- src/component/Project.js
+++ src/component/Project.js
@@ -105,19 +105,6 @@ function Project() {
 
                     )
                 })}
-                // {projectlist?.length > 0 && (
-                //     <motion.div
-                //         initial={{ x: -100, opacity: 0 }}
-                //         animate={{ x: 0, opacity: 1 }}
-                //         transition={{ duration: 0.5, delay: 0.5 }}
-                //         className='flex items-center gap-2'>
-                //         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                //             <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                //         </svg>
-                //         <Link to="/newproject"  >Add new project</Link>
-
-                //     </motion.div>
-                // )}
 
             </div>
         </div>
